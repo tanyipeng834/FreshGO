143672a8259ead6a5c38f857b9fbb1d10e0738cc
JermYong <97948242+JermYong@users.noreply.github.com>
2023-03-08 23:08:02 +0800

    Update purchase_activity.sql

--- databases/purchase_activity.sql
+++ databases/purchase_activity.sql
@@ -19,35 +19,33 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: 'order'
+-- Database: `order`
 --
-CREATE DATABASE IF NOT EXISTS 'PURCHASE_ACTIVITY' DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE 'PURCHASE_ACTIVITY';
+CREATE DATABASE IF NOT EXISTS `PURCHASE_ACTIVITY`DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `PURCHASE_ACTIVITY`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table 'order'
+-- Table structure for table `order`
 --
 
-DROP TABLE IF EXISTS 'purchase_activity';
-CREATE TABLE IF NOT EXISTS 'purchase_activity' (
-  'purchase_id' int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  'customer_id' varchar(32) NOT NULL,
-  'crop_purchase' varchar[] NOT NULL,
-  'delivery_staff_id' int(11) NOT NULL,
-  'customer_location' varchar(50) NOT NULL,
-  'transaction_amount' float(10) NOT NULL,
-  'status' varchar(10) NOT NULL DEFAULT 'Ongoing/New',
-  'created' timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  'modified' timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY ('purchase_id')
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `purchase_activity`;
+CREATE TABLE IF NOT EXISTS `purchase_activity`(
+  `purchase_id`int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `customer_id`varchar(32) NOT NULL,
+  `crop_purchase`varchar(32) NOT NULL,
+  `delivery_staff_id`int(11) NOT NULL,
+  `customer_location`varchar(50) NOT NULL,
+  `transaction_amount`float(10) NOT NULL,
+  `status`varchar(32) NOT NULL DEFAULT 'Ongoing/New',
+  `created`timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;;
 
 --
 
-/*INSERT INTO 'purchase' ('purchase_id', 'customer_id', 'status', 'created', 'modified') VALUES
-(1, 'Apple TAN', 'NEW', '2020-06-12 02:14:55', '2020-06-12 02:14:55'); */
+/*INSERT INTO `purchase`(`purchase_id`, `customer_id`, `status`, `created`, `modified`) VALUES
+(1, `Apple TAN`, `NEW`, `2020-06-12 02:14:55`, `2020-06-12 02:14:55`); */
 
 -- --------------------------------------------------------
 --
