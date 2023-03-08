a1c2016cdad1b1c5f3bd7fc7beaffdfca003f216
JermYong <97948242+JermYong@users.noreply.github.com>
2023-03-08 17:24:33 +0800

    Update purchase_activity.sql

--- databases/purchase_activity.sql
+++ databases/purchase_activity.sql
@@ -52,34 +52,6 @@ CREATE TABLE IF NOT EXISTS 'purchase_activity' (
 -- --------------------------------------------------------
 --
 
-DROP TABLE IF EXISTS 'order_item';
-CREATE TABLE IF NOT EXISTS 'order_item' (
-  'item_id' int(11) NOT NULL AUTO_INCREMENT,
-  'purchase_id' int(11) NOT NULL,
-  'book_id' char(13) NOT NULL,
-  'quantity' int(11) NOT NULL,
-  PRIMARY KEY ('item_id'),
-  KEY 'FK_order_id' ('order_id')
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table 'order_item'
---
-
-INSERT INTO 'order_item' ('item_id', 'order_id', 'book_id', 'quantity') VALUES
-(1, 1, '9781434474234', 1),
-(2, 1, '9781449474212', 1);
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table 'order_item'
---
-ALTER TABLE 'order_item'
-  ADD CONSTRAINT 'FK_order_id' FOREIGN KEY ('order_id') REFERENCES 'order' ('order_id') ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
