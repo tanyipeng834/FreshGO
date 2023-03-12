8aff77f279ee97c897a83dac505fdffa53fbab74
cal-lee <114975331+cal-lee@users.noreply.github.com>
2023-03-12 18:55:38 +0800

    testing

---
+++ client/src/fakeProductsData.js
@@ -0,0 +1,33 @@
+// this is just a sample data to ensure that the front end works, replace with data from database
+import caixinhua from './assets/caixinhua';
+import thaibasil from './assets/thaibasil';
+import kangkong from './assets/kangkong';
+
+export const products = [{
+    id: '1',
+    name: 'Cai Xin Hua',
+    shelfLife: '2023-12-31',
+    price: '2.00',
+    quantityInStock: '10',
+    imageURL: caixinhua,
+}, {
+    id: '2',
+    name: 'Thai Basil',
+    shelfLife: '2023-12-31',
+    price: '2.50',
+    quantityInStock: '5',
+    imageURL: thaibasil,
+}, {
+    id: '3',
+    name: 'Kang Kong',
+    shelfLife: '2023-12-31',
+    price: '1.50',
+    quantityInStock: '20',
+    imageURL: kangkong,
+}];
+
+// referencing the above products array
+export const cartItems = [
+    products[0],
+    products[1]
+];
--- client/src/views/CartPage.vue
+++ client/src/views/CartPage.vue
@@ -9,7 +9,14 @@ export default {
 </script>
 
 <style scoped>
-h1{
-    color: green;
-}
+    h1 {
+        border-bottom: 1px solid black;
+        margin: 0;
+        margin-top: 16px;
+        padding: 16px;
+    }
+
+    #total-price {
+        margin:0;
+    }
 </style>
