f56e8bf577fac4ddbca4bd651dd71817bafaa383
T-Leng <tleng.eng.2021@scis.smu.edu.sg>
2023-03-13 16:09:58 +0800

    Added log in pages

---
+++ client/src/views/Secure.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="secure">
+    <h1>This is the profile page</h1>
+  </div>
+</template>
+
+
+<script>
+    export default {
+        name: 'Secure',
+        data() {
+          return {}
+        },
+        mounted() {
+            if(!this.$parent.authenticated) {
+                this.$router.replace({ name: "Login" });
+            }
+        },
+        methods: {
+        }
+    }
+</script>
---
+++ client/src/views/Signup.vue
@@ -0,0 +1,67 @@
+<template>
+    <div id="signup">
+        <h1>Sign Up</h1>
+        <div class="form-inputs">
+            <label for="email">Email</label>
+            <input type="text" id="email" name="email" v-model="input.email" placeholder="Email" />
+        </div>
+        <div class="form-inputs">
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" v-model="input.password" placeholder="Password" />
+        </div>
+        <div class="form-inputs">
+            <label for="password">Confirm Password</label>
+            <input type="password" id="cpassword" name="cpassword" v-model="input.cpassword" placeholder="Confirm Password" />
+        </div>
+        <div class="form-inputs">
+            User Type:<input type="radio" id="Farmer" name="usertype" v-model="input.usertype" value="Farmer">
+            <label for='Farmer'>Farmer</label>
+            <input type="radio" id="Customer" name="usertype" v-model="input.usertype" value="Customer">
+            <label for='Customer'>Customer</label>
+        </div>
+        <button type="button" v-on:click="signup()">Sign Up</button>
+        <br>
+        Already have an account? Log in <router-link to="/Login">Here</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Login',
+        data() {
+            return {
+                input: {
+                    username: "",
+                    password: ""
+                }
+            }
+        },
+        methods: {
+            login() {
+                if(this.input.username != "" && this.input.password != "") {
+                    // This should actually be an api call not a check against this.$parent.mockAccount
+                    if(this.input.username == this.$parent.mockAccount.username && this.input.password == this.$parent.mockAccount.password) {
+                        this.$emit("authenticated", true);
+                        this.$router.replace({ name: "Secure" });
+                    } else {
+                        console.log("The username and / or password is incorrect");
+                    }
+                } else {
+                    console.log("A username and password must be present");
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+
+#signup .form-inputs {
+    padding-bottom: 10px;
+}
+
+#signup .form-inputs label {
+    padding-right: 10px;
+}
+
+</style>
