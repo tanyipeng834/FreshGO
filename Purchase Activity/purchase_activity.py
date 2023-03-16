27e119345b178f8e306d9ca9d134eaa91c7687bd
JermYong <jeremyyong13@gmail.com>
2023-03-16 16:18:16 +0800

    commit message

--- Purchase Activity/purchase_activity.py
+++ Purchase Activity/purchase_activity.py
@@ -1,48 +1,130 @@
 #!/usr/bin/env python3
 # The above shebang (#!) operator tells Unix-like environments
 # to run this file as a python3 script
-
-
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-import mysql.connector as sql
+import os
+from datetime import datetime
+import json 
+from invokes import invoke_http
+
+# book_URL = "http://localhost:5000/book"
+# order_URL = "http://localhost:5001/order"
+# shipping_record_URL = "http://localhost:5002/shipping_record"
+# activity_log_URL = "http://localhost:5003/activity_log"
+# error_URL = "http://localhost:5004/error"
 
 app = Flask(__name__)
 CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] ='mysql+mysqlconnector://root@localhost:3306/purchase_request'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+class Purchase_Activity(db.Model):
+    __tablename__ = 'purchase_activity'
+    id = db.Column(db.Integer, nullable=False, primary_key=True)
+    customer_id = db.Column(db.Integer, nullable=False)    
+    customer_location = db.Column(db.Integer)
+    status = db.Column(db.String, nullable = False)
+    created = db.Column(db.DateTime, nullable=False)
+
+    def json(self):
+        dto={"Purchase ID": self.id, "Customer ID": self.customer_id,
+                "Customer Location": self.customer_location, 
+                "Status": self.status, "Created": self.created}
+        dto['crop_purchased']=[]
+        for item in self.abc:
+            dto['crop_purchased'].append(item.json())
+        return dto
+    
+class Crop_Purchased(db.Model):
+    __tablename__ = 'crop_purchased'
+    id = db.Column(db.Integer, nullable = False, primary_key=True)
+    crop_id = db.Column(db.Integer, nullable = False)
+    quantity=db.Column(db.Integer, nullable=False)
+    purchase_id = db.column(db.ForeignKey('purchase_activity.id',ondelete='cascade',onupdate='cascade'))
+    #purchase_id=db.relationship('Purchase_Activity', foreign_keys=[id])
+
+    def __init__(self, crop_id, quantity):
+        self.crop_id= crop_id,
+        self.quantity=quantity
+
+    def json(self):
+        return {"ID": self.id, "Crop ID": self.crop_id, 
+                "Quantity":self.quantity, "Purchase ID": self.purchase_id}
 
 @app.route("/purchase_request", methods=['POST'])
-def receiveLog():
-    # Check if the request contains valid JSON
-    log = None
-    if request.is_json:
-        log = request.get_json()
-        processLog(log)
-        # reply to the HTTP request
-        return jsonify({"code": 200, "data": 'OK. Activity log printed.'}), 200 # return message; can be customized
-    else:
-        log = request.get_data()
-        print("Received an invalid log:")
-        print(log)
-        print()
-        return jsonify({"code": 400, "message": "Activity log input should be in JSON."}), 400 # Bad Request
+def create_request():
+    cart_item=request.json.get('cart_item')
+    customer_id = request.json.get('customer_id', None)
+    customer_location = request.json.get('customer_location', None)
+    create_request = Purchase_Activity(customer_id=customer_id, customer_location=customer_location)
+    for item in cart_item:
+        create_request.abc.append(Crop_Purchased(crop_id=item['crop_id'],
+                                                            quantity=item['quantity']))
+    try:
+        db.session.add(create_request)
+        db.session.commit()
 
-def processLog(purchase_request):
-    conn=sql.connect(host='localhost',user='root', password='',database='PURCHASE_ACTIVITY')
-    print("Recording a log:")
-    cursor= conn.cursor()
-    sql="INSERT INTO purchase_activity (customer_id, crop_purchase, delivery_staff_id, customer_location, transaction_amount) VALUES (%s, %s)"
-    customer_id=0
-    crop_purchase=[]
-    delivery_staff_id=0
-    customer_location=''
-    transaction_amount=0
-    val=(customer_id, crop_purchase, delivery_staff_id, customer_location, transaction_amount)
-    cursor.execute(sql, val)
-    conn.commit()
-    print() # print a new line feed as a separator
-    print(cursor.rowcount,'record inserted')
+    except:
+        return jsonify(
+                    {
+                    "code": 500,
+                    "data": {
+                    create_request.json()
+                    },
+                    "message": "An error occurred creating the purchase request."
+                    }
+                ), 500 
+    print("Order Confirmed, Looking for Driver")
+    return jsonify(
+        {  "code": 201,
+        "data": create_request.json()
+        }
+        ),
+   
+
+# @app.route("/purchase_request/order", methods=['GET','POST'])
+# def purchase_request(customer_id, delivery_staff_id, customer_location,transaction_amount, status, created):
+#     data = request.get_json()
+#     #create request for customer's purchase
+#     purchase_request = Purchase_Activity(customer_id, delivery_staff_id, customer_location,transaction_amount, status, created,**data)
+#     try:
+#         db.session.add(purchase_request)
+#         db.session.commit()
+#     except:
+#         return jsonify(
+#             {
+#                 "code": 500,
+#                 "data": {
+#                     "Customer_ID": customer_id,
+#                     "Delivery Staff":delivery_staff_id,
+#                     "Delivery Location":customer_location,
+#                     "Transaction Amt":transaction_amount,
+#                     "Purchase Request ID": id,
+#                     "Customer ID": customer_id,
+#                     "Status":status
+#                     },
+#                 "message": "An error occurred creating the purchase request."
+#             }
+#         ), 500
+#     return jsonify(
+#         {  "code": 201,
+#             "data": Purchase_Activity.json()
+#         }
+#     ), 201
+
+
 
 
 if __name__ == "__main__":  # execute this program only if it is run as a script (not by 'import')
     print("This is flask for " + os.path.basename(__file__) + ": recording logs ...")
-    app.run(host='0.0.0.0', port=5003, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+    print("Goodbye~")
+
+
+#!/usr/bin/env python3
+# The above shebang (#!) operator tells Unix-like environments
+# to run this file as a python3 script
+
--- inventory/inventory.py
+++ inventory/inventory.py
@@ -215,4 +215,4 @@ def crop_data_controller(crop_name, batch):
         # Now we will create a crop object
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-    print("Hello World")
+    print("GoodBye!")
