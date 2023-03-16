73e5513b757a9237f60441a19a393046eb2f845c
cal-lee <114975331+cal-lee@users.noreply.github.com>
2023-03-16 17:25:18 +0800

    debugged

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -6,12 +6,10 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Home from '@/components/Home.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
