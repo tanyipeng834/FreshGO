1797487e2286440e6a16b94f294c9ea10397fdbf
T-Leng <tleng.eng.2021@scis.smu.edu.sg>
2023-03-19 23:28:07 +0800

    dashboard

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -64,6 +64,16 @@ const routes = [
     component: HomeView,
   },
   {
+    path: '/dashboard',
+    name: 'dashboard',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: function () {
+      return import(/* webpackChunkName: "about" */ '../views/Dashboard.vue')
+    }
+  },
+  {
     path: '/Entry',
     name: 'Entry',
     component: function () {
