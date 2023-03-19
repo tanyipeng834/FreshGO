067285baec3c0458b8c128ec3e791b88d71f9eb5
Yi Peng <tanyipeng835@hotmail.com>
2023-03-19 23:51:17 +0800

    working on the frontend

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,81 +1,24 @@
 <template>
-    <div id="login">
-        <h1>Login</h1>
-        <div class="form-inputs">
-            <input type="radio" id="Farmer" name="usertype" v-model="input.usertype" value="Farmer">
-            <label for='Farmer'>Farmer</label>
-            <input type="radio" id="Customer" name="usertype" v-model="input.usertype" value="Customer">
-            <label for='Customer'>Customer</label>
-            <input type="radio" id="staff" name="usertype" v-model="input.usertype" value="staff">
-            <label for='staff'>Staff</label>
-        </div>
-        <div class="form-inputs">
-            <label for="email">Email</label>
-            <input type="text" id="email" name="email" v-model="input.email" placeholder="email@example.com" />
-        </div>
-        <div class="form-inputs">
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password" v-model="input.password" placeholder="Password" />
-        </div>
-        <button type="button" v-on:click="login()">Login</button>
-        <br>
-        Don't have an account? Sign up <router-link to="/Signup">Here</router-link>
-    </div>
-
-    <!--<div class="dropdown-menu">
-    <form class="px-4 py-3">
-        <div class="mb-3">
-        <label for="email" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="email" name="email" v-model="input.email" placeholder="email@example.com">
-        </div>
-        <div class="mb-3">
-        <label for="password" class="form-label">Password</label>
-        <input type="password" class="form-control" id="password" name="password" v-model="input.password" placeholder="Password">
-        </div>
-        <button type="button" v-on:click="login()" class="btn btn-success">Sign in</button>
-    </form>
-    <div class="dropdown-divider"></div>
-    Don't have an account? Sign up <router-link to="/Signup">Here</router-link>
-    <a class="dropdown-item" href="#">Forgot password?</a>
-</div>-->
+  <!-- Default dropup button -->
+  <ul class="dropdown-menu">
+    <li><h6 class="dropdown-header">Dropdown header</h6></li>
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">Another action</a></li>
+  </ul>
 </template>
 
 <script>
-    export default {
-        name: 'Login',
-        data() {
-            return {
-                input: {
-                    username: "",
-                    password: ""
-                }
-            }
-        },
-        methods: {
-            login() {
-                if(this.input.username != "" && this.input.password != "") {
-                    // This should actually be an api call not a check against this.$parent.mockAccount
-                    if(this.input.username == this.$parent.mockAccount.username && this.input.password == this.$parent.mockAccount.password) {
-                        this.$emit("authenticated", true);
-                        this.$router.replace({ name: "Secure" });
-                    } else {
-                        console.log("The username and / or password is incorrect");
-                    }
-                } else {
-                    console.log("A username and password must be present");
-                }
-            }
-        }
-    }
+export default {
+  name: "Login",
+};
 </script>
 
 <style>
-
 #login .form-inputs {
-    padding-bottom: 10px;
+  padding-bottom: 10px;
 }
 
 #login .form-inputs label {
-    padding-right: 10px;
+  padding-right: 10px;
 }
 </style>
--- client/src/views/Signup.vue
+++ client/src/views/Signup.vue
@@ -1,78 +1,19 @@
 <template>
-    <div id="signup">
-        <h1>Sign Up</h1>
-        <div class="form-inputs">
-            <label for="email">Email</label>
-            <input type="text" id="email" name="email" v-model="input.email" placeholder="Email" />
-        </div>
-        <div class="form-inputs">
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password" v-model="input.password" placeholder="Password" />
-        </div>
-        <div class="form-inputs">
-            <label for="cpassword">Confirm Password</label>
-            <input type="password" id="cpassword" name="cpassword" v-model="input.cpassword" placeholder="Confirm Password" />
-        </div>
-        <div class="form-inputs">
-            <label for="password">Confirm Password</label>
-            <input type="password" id="cpassword" name="cpassword" v-model="input.cpassword" placeholder="Confirm Password" />
-        </div>
-        <div>
-    <router-link
-      to="/MoreInfo"
-      custom
-      v-slot="{ navigate }"
-    >
-      <button
-        @click="navigate"
-        role="link"
-      >
-        Signup
-      </button>
-    </router-link>
-  </div>
-        <br>
-        Already have an account? Log in <router-link to="/Login">Here</router-link>
-    </div>
+  
 </template>
 
 <script>
-    export default {
-        name: 'Login',
-        data() {
-            return {
-                input: {
-                    username: "",
-                    password: ""
-                }
-            }
-        },
-        methods: {
-            login() {
-                if(this.input.username != "" && this.input.password != "") {
-                    // This should actually be an api call not a check against this.$parent.mockAccount
-                    if(this.input.username == this.$parent.mockAccount.username && this.input.password == this.$parent.mockAccount.password) {
-                        this.$emit("authenticated", true);
-                        this.$router.replace({ name: "Secure" });
-                    } else {
-                        console.log("The username and / or password is incorrect");
-                    }
-                } else {
-                    console.log("A username and password must be present");
-                }
-            }
-        }
-    }
+export default {
+  name: "Login",
+};
 </script>
 
 <style>
-
 #signup .form-inputs {
-    padding-bottom: 10px;
+  padding-bottom: 10px;
 }
 
 #signup .form-inputs label {
-    padding-right: 10px;
+  padding-right: 10px;
 }
-
 </style>
