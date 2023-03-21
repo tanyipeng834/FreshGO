686bb92442d4594b7cc40d87b15c0f796ff6c3c3
cal-lee <114975331+cal-lee@users.noreply.github.com>
2023-03-21 20:35:40 +0800

    Show distance & price

--- client/src/views/DeliveryStaffUI/DeliveryRequestDetails.vue
+++ client/src/views/DeliveryStaffUI/DeliveryRequestDetails.vue
@@ -1,11 +1,56 @@
 <template>
-    <h1>This is Delivery Request Details Page</h1>
-    <!--Google maps API-->
+  
+    <h1>Delivery Distance in m: {{ travel }}m</h1>
+    <h1>Delivery Distance in km: {{ toKm }}km</h1>
+    <h1>Delivery Price: ${{ deliveryPrice }}</h1>
+
 </template>
     
 <script>
+import axios from "axios"
 export default {
     name: "DeliveryRequestDetails",
+    data()
+    {
+        return {
+            //travel: this.travel,
+            travel: {},
+            //deliveryPrice: {},
+        };
+    },
+
+    mounted() {
+        axios
+            .get("https://api.distancematrix.ai/maps/api/distancematrix/json?origins=2 Gambas Cres, Singapore 757044&destinations=9 Bishan Pl, Singapore 579837&key=DnWDU3yyV9JiLvLEEq61ZGWdVUGXD")
+            // .then(response => {this.distance = response.data.rows[0].elements[0].distance})
+            .then(response => {
+                // console.log(response.data.rows[0].elements[0].distance)
+                this.travel = response.data.rows[0].elements[0].distance.value
+                //console.log(this.travel)
+            })
+            .catch(error => {
+                console.log(error.message)
+           });
+    },
+
+    computed: {
+        // first 3km, flat rate $2, every km after, add $0.20
+        deliveryPrice: function() {
+            if(this.travel<=3000) {
+                return 2.00;
+            } else{
+                if(((this.travel - 3000)/1000).toFixed(0)<1){
+                    return 2 + 0.20;
+                } else {
+                    return (2 + ((this.travel - 3000)/1000).toFixed(0) * 0.2).toFixed(2);
+                }
+            }
+        },
+
+        toKm: function() {
+            return (this.travel/1000).toFixed(2);
+        },
+    },
 } ;
 </script>
 
