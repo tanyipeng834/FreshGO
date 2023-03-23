548ae45008b359f71be8dcac2193d64731a35fe0
MoSengT <moseng.tim.2021@smu.edu.sg>
2023-03-23 20:40:10 +0800

    Left invoking purchase activity

--- databases/crop_management.sql
+++ databases/crop_management.sql
@@ -16,3 +16,42 @@ CREATE TABLE CropData (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
+-- PREVIOUS TABLE FOR REFERENCE
+
+-- DROP TABLE IF EXISTS `CropData`;
+-- CREATE TABLE IF NOT EXISTS `CropData` (
+--     `name` varchar(30) NOT NULL,
+--     `batch` int(5) NOT NULL AUTO_INCREMENT,
+--     `humidity` decimal(10,2) NOT NULL,
+--     `water` decimal (10,2) NOT NULL,
+--     `fertiliser` decimal(10,2) NOT NULL,
+--     PRIMARY KEY(`batch`)
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- DROP TABLE IF EXISTS `CropMeasurements`;
+-- CREATE TABLE IF NOT EXISTS `CropMeasurements` (
+--     `name` varchar(30) NOT NULL,
+--     `batch` int(5) NOT NULL,
+--     `date_measured` DATE NOT NULL,
+--     `current_height` decimal(10,2) NOT NULL,
+--     PRIMARY KEY(`name`),
+--     FOREIGN KEY (`name`) REFERENCES `inventory`(`name`) ON DELETE CASCADE
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+-- INSERT INTO `CropData` (`name`, `batch`, `humidity`, `water`, `fertiliser`) VALUES
+-- ('Xin Hua', 1, 2.0, 3.0, 3.0),
+-- ('Xin Hua', 2, 2.0, 2.0, 2.0),
+-- ('Xin Hua', 3, 2.0, 3.0, 2.0),
+-- ('Xin Hua', 4, 2.0, 5.0, 3.0),
+-- ('Xin Hua', 5, 2.0, 1.0, 4.0);
+-- COMMIT;
+
+-- INSERT INTO `CropMeasurements` (`name`, `batch`, `date_measured`, `current_height`) VALUES
+-- ('Xin Hua', 1, '2021-02-02', 3.0),
+-- ('Xin Hua', 2, '2021-02-03', 3.0),
+-- ('Xin Hua', 3, '2021-02-04', 4.0),
+-- ('Xin Hua', 4, '2021-02-05', 3.0),
+-- ('Xin Hua', 5, '2021-02-06', 2.0);
+-- COMMIT;
--- inventory/inventory.py
+++ inventory/inventory.py
@@ -20,59 +20,24 @@ db = SQLAlchemy(app)
 class Inventory(db.Model):
     __tablename__ = 'inventory'
     name = db.Column(db.String(15), nullable=False, primary_key=True)
-    shell_life = db.Column(db.String(15), nullable=False)
-    price = db.Column(db.Float(precision=2), nullable=False)
-    quantity = db.Column(db.Integer, nullable=False)
-    height = db.Column(db.Float(precision=2))
-    # May not need batch for customer not important only needed for farmer
-    batch = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.String(15), nullable=False)
+    quantity = db.Column(db.Float, nullable=False)
+    status = db.Column(db.String(15), nullable=False)
 
-    # Add the init method into the class
-    # def __init__(self, name, shell_life, price, quantity, height, type):
-    #     self.name = name
-    #     self.shell_life =shell_life
-    #     self.price = price
-    #     self.quantity = quantity
-    #     self.height = height
-    #     self.type = type
+    def __init__(self, name, quantity):
+        self.name = name
+        self.quantity = quantity
+        if self.quantity <= 10:
+            self.status = "low"
+        elif self.quantity <= 15:
+            self.status = "medium"
+        else:
+            self.status = "high"
 
     def json(self):
-        return {"CropName": self.name, "Shell Life": self.shell_life, "Price": self.price, "Quantity": self.quantity, "Height": self.height, "Type": self.type}
+        return {"CropName": self.name, "Quantity": self.quantity, "Status": self.status}
+    
 
-
-class CropData(db.Model):
-    __tablename__ = 'CropData'
-    name = db.Column(db.String(15), db.ForeignKey(
-        'inventory.name'), nullable=False, primary_key=True)
-    batch = db.Column(db.Integer, db.ForeignKey(
-        'inventory.batch'), nullable=False, primary_key=True)
-    humidity = db.Column(db.Float(precision=2), nullable=False)
-    water = db.Column(db.Float(precision=2), nullable=False)
-    fertiliser = db.Column(db.Float(precision=2), nullable=False)
-    crop_name = db.relationship("Inventory", foreign_keys=[name])
-    crop_batch = db.relationship("Inventory", foreign_keys=[batch])
-
-    def json(self):
-        return {"Crop Name": self.name, "Crop Batch": self.batch, "Humidity Levels": self.humidity, "Water Level": self.water, "Fertiliser": self.fertiliser}
-
-
-class CropMeasurements(db.Model):
-    __tablename__ = 'CropMeasurements'
-    name = db.Column(db.String(15), db.ForeignKey(
-        'inventory.name'), nullable=False, primary_key=True)
-    batch = batch = db.Column(db.Integer, db.ForeignKey(
-        'inventory.batch'), nullable=False, primary_key=True)
-    date_measured = db.Column(db.Date, primary_key=True)
-    current_height = db.Column(db.Float(precision=2), nullable=False)
-    crop_name = db.relationship("Inventory", foreign_keys=[name])
-    crop_batch = db.relationship("Inventory", foreign_keys=[batch])
-
-    def json(self):
-        return {"Crop Name": self.name, "Crop Batch": self.batch, "Date Measured": self.date_measured, "Crop Height": self.current_height}
-
-
-@app.route("/inventory", methods=["GET", "POST"])
+@app.route("/inventory", methods=["GET", "POST", "PUT"])
 def get_all_crops():
     if request.method == "GET":
 
@@ -92,14 +57,13 @@ def get_all_crops():
                 "message": "There are no crops."
             }
         ), 404
-    else:
-
+    elif request.method=="POST":
         # This will allow the farmer to create a crop in the database
         data = request.get_json()
         # crop_name = data['name']
-        batch_no = data['batch']
+        crop_name = data['name']
         # Check if there is a crop with a similar name in the database
-        if not (Inventory.query.filter_by(batch=batch_no).first()):
+        if not (Inventory.query.filter_by(name=crop_name).first()):
             # Create a new object based on the input
             crop = Inventory(**data)
             print(crop)
@@ -133,84 +97,36 @@ def get_all_crops():
                     "message": "Crop Batch already exists."
                 }
             ), 400
-
-
-@app.route("/inventory/measurements/<string:crop_name>", methods=["GET"])
-def get_crop_measurements(crop_name):
-    # Get all the measurements for the crop with crop_name
-
-    # Use an inner join to connect both the measurements and the input
-    query = db.session.query(CropMeasurements, CropData).join(CropData, db.and_(
-        CropMeasurements.name == CropData.name, CropMeasurements.batch == CropData.batch))
-    crops_data = query.filter(CropData.name == crop_name).all()
-    if len(crops_data):
-        return jsonify(
-            {
-                "code": 200,
-                "data": {
-                    "Crop Data": [{"Crop Name": crop_measurements.name, "Crop Batch": crop_measurements.batch, "Date Measured": crop_measurements.date_measured, "Crop Height": crop_measurements.current_height, "Crop Humidity": crop_data.humidity, "Crop Water Level": crop_data.water, "Crop Fertiliser": crop_data.fertiliser} for crop_measurements, crop_data in crops_data]
-                }
-            }
-        )
-    else:
-        return jsonify(
-            {
-                "code": 404,
-                "message": "There is not mesurements for this crop"
-
-            }
-
-        )
-
-
-@app.route("/inventory/<string:crop_name>/<string:batch>", methods=["GET", "POST"])
-def crop_data_controller(crop_name, batch):
-    if request.method == "GET":
-        # Get the data for the input parmaters
-        crop_data = Inventory.query.filter_by(name=crop_name, batch=batch)
-        if len(crop_data):
-            return jsonify(
-                {
-                    "code": 200,
-                    "data": {
-                        "Crop Data": crop_data.json()
-                    }
-                }
-            )
-        return jsonify(
-            {
-                "code": 404,
-                "message": f"There are no data for this ${crop_name} belonging to this ${batch}."
-            }
-        ), 404
-    else:
-
-        # This will allow the farmer to create a crop in the database
-        data = request.get_json()
+    elif request.method=="PUT":
         # Check if there is a crop with a similar name in the database
-        if not (CropData.query.filter_by(name=crop_name, batch=batch).first()):
+        data = request.get_json()
+        crop_name = data['name']
+        if (Inventory.query.filter_by(name=crop_name).first()):
             # Create a new object based on the input
-            crop_data = CropData(name=crop_name, batch=batch, **data)
+            details = Inventory.query.filter_by(name=crop_name).first()
+            data['quantity'] = data['quantity'] + details.quantity
+            crop = Inventory(**data)
 
             try:
-                db.session.add(crop_data)
+                db.session.delete(details)
+                db.session.commit()
+                db.session.add(crop)
                 db.session.commit()
             except:
                 return jsonify(
                     {
                         "code": 500,
                         "data": {
-                            "Crop Name": crop_name,
-                            "Crop Batch": batch
+                            "Crop Name": data['name']
                         },
-                        "message": f"An error occurred creating batch ${batch} of this ${crop_name}."
+                        "message": "An error occurred when updating the Inventory."
                     }
                 ), 500
 
             return jsonify(
                 {
                     "code": 201,
-                    "data": crop_data.json()
+                    "data": crop.json()
                 }
             )
         else:
@@ -218,20 +134,11 @@ def crop_data_controller(crop_name, batch):
                 {
                     "code": 400,
                     "data": {
-                        "Crop Name": crop_name,
-                        "Crop Batch": batch
+                        "Crop Name": data['name']
                     },
-                    "message": "Crop Batch already exists."
+                    "message": "Crop Batch doesn't exists."
                 }
             ), 400
 
-
-@app.route("/inventory/check-inventory", methods=["GET"])
-def check_crop_quantity():
-    # Get all the measurements for the crop with quantity less than 5
-    Inventory.query()
-
-    # Use an inner join to connect both the measurements and the input
-    # Now we will create a crop object
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
